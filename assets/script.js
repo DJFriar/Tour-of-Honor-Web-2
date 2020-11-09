0d84e7cfe17dc175b7f673706d30806238fe0a8d
DJFriar <tommy.craft@icloud.com>
2020-11-08 20:54:17 -0600

    portal flow updates

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,30 @@
-// $(".warningTag").on("click", function() {
-//     alert("Duplicate Entry Detected");
+// Google Maps Support
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: 34.97959, lng: -101.928469 },
+//     zoom: 18,
+//   });
+// }
+
+// Apple Maps Support
+// mapkit.init({
+//     authorizationCallback: function(done) {
+//         done('eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IkY0VUo2Mks1V1EifQ.eyJpc3MiOiI4WEhZN0VRN1RFIiwiaWF0IjoxNjA0ODgwODczLCJleHAiOjE2MzE2NjQ4NzN9.lB16PGBWkiQpDrjSuDjpqhTbKPoHAcuzd03Lcjrvt-wS_x2aHBC4R3m58plMQ0l0TwLcB8Ujv-JyWBqhZdmOuA');
+//     }
 // });
+
+// var MarkerAnnotation = mapkit.MarkerAnnotation
+// var photoCoords = new mapkit.Coordinate(34.97959, -101.928469)
+
+// var demoRegion = new mapkit.CoordinateRegion(
+//     new mapkit.Coordinate(34.97959, -101.928469),
+//     new mapkit.CoordinateSpan(0.167647972, 0.354985255)
+// );
+// var map = new mapkit.Map("map");
+
+// var locationAnnotation = new MarkerAnnotation(photoCoords, { color: "#ffff00", title: "DB127", glyphText: "📷" });
+// map.showItems([locationAnnotation]);
+
+// map.region = demoRegion;
